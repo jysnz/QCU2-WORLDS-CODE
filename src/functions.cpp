0fb87083b4efb568c0d9923ceb99f033e38fa72d
Jayson Dela Cruz <delacruzjayson020305@gmail.com>
2026-04-24 13:32:08 +0800

    Almost got the homing

--- src/functions.cpp
+++ src/functions.cpp
@@ -177,7 +177,14 @@ void armGateHomingTask(void *param) {
   gate.tare_position();
 
   // Move arm to the resting position
-  arm.move_relative(2200, 200);
+  arm.move_relative(2900, 200);
+  pros::delay(1000);
+  arm.move_relative(-200, 200);
+  pros::delay(500);
+  arm.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  pros::delay(200);
+  // arm.move_relative(-200, 200);
+  // pros::delay(300);
   arm.tare_position();
 
   gateClose();
